c7cca125fd70fc962903ac70e3867f61de55458c
AlekseiEvgenievich <aleksei.smirnov@phystech.edu>
2023-12-28 23:19:36 +0100

    vv

--- vk_api.py
+++ vk_api.py
@@ -1,52 +1,37 @@
 import os
 import random
+from pathlib import Path
 
 import requests
 from dotenv import load_dotenv
 from telegram import Bot
 
 
-def download_image(url, name_path='images/comic.png'):
-    response = requests.get(url)
-    response.raise_for_status()
-    with open(name_path, 'wb') as file:
-        file.write(response.content)
-        
-        
-def send_on_telegram(bot_token, folder_name, channel_name, funny_comment):
+def send_on_telegram(bot_token, outpath, channel_name, funny_comment):
     max_file_size = 20 * 1024 * 1024
     bot = Bot(token=bot_token)
-    for dirpath, dirnames, filenames in os.walk(folder_name):
-        for file_name in filenames:
-            file_path = '{}/{}'.format(folder_name, file_name)
-            if os.path.getsize(file_path) <= max_file_size:
-                with open(f'{folder_name}/{file_name}', 'rb') as photo_file:
-                    bot.send_photo(chat_id=channel_name, photo=photo_file)
-                    bot.send_message(chat_id=channel_name, text=funny_comment)
-            os.remove(file_path)
-                
+    if os.path.getsize(outpath) <= max_file_size:
+        with open(outpath, 'rb') as photo_file:
+            bot.send_photo(chat_id=channel_name, photo=photo_file)
+            bot.send_message(chat_id=channel_name, text=funny_comment)
 
-def get_random_number_of_comics():
+
+def download_random_comic(folder_name):
     current_url = 'https://xkcd.com/info.0.json'
-    response = requests.get(current_url)
-    response.raise_for_status()
-    max_number_photo = int(response.json()['num'])
-    random_number = random.randint(0, max_number_photo)
-    return random_number
-
-
-def get_image_link(number_of_comics):
-    comics_url = f'https://xkcd.com/{number_of_comics}/info.0.json'
-    response = requests.get(comics_url)
-    response.raise_for_status()
-    return response.json()['img']
-    
-    
-def get_funny_comment(number_of_comics):
-    comics_url = f'https://xkcd.com/{number_of_comics}/info.0.json'
-    response = requests.get(comics_url)
-    response.raise_for_status()
-    return response.json()['alt']
+    current_response = requests.get(current_url)
+    current_response.raise_for_status()
+    max_photo_number = int(current_response.json()['num'])
+    random_number = random.randint(0, max_photo_number)
+    comics_url = f'https://xkcd.com/{random_number}/info.0.json'
+    comics_response = requests.get(comics_url)
+    comics_response.raise_for_status()
+    image_response = requests.get(comics_response.json()['img'])
+    image_response.raise_for_status()
+    file_name = f'xcd_{random_number}'
+    outpath = Path.cwd() / folder_name / file_name
+    with open(outpath, 'wb') as file:
+        file.write(image_response.content)
+    return outpath, comics_response.json()['alt']
 
 
 if __name__ == '__main__':
@@ -55,7 +40,8 @@ if __name__ == '__main__':
     folder_name = os.environ["IMAGE_NAME_FOLDER"]
     channel_name = os.environ["TELEGRAM_CHANNEL_NAME"]
     
-    random_number_of_comic = get_random_number_of_comics()
-    download_image(get_image_link(random_number_of_comic))
-    funny_comment = get_funny_comment(random_number_of_comic)
-    send_on_telegram(bot_token, folder_name, channel_name, funny_comment)
+    outpath,funny_comment = download_random_comic(folder_name)
+    try:
+        send_on_telegram(bot_token, outpath, channel_name, funny_comment)
+    finally:
+        os.remove(outpath)
